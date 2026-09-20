0ef5b3c6cdf2b0ac2f965258688c4ab94e5fa4f6
Matheus Silva Magalhães <matheusmagdevelop@gmail.com>
2026-09-20 19:46:41 -0300

    fix: corrigindo o hover dos cards

--- app/utils/ToolsCard.tsx
+++ app/utils/ToolsCard.tsx
@@ -5,7 +5,7 @@ type ToolsProps = {
 
 export default function ToolsCard({title, tools}: ToolsProps){
   return(
-    <section className="flex flex-col items-start justify-start gap-6 bg-[#0d0d14] h-40 px-4 py-4 rounded-lg">
+    <section className="flex flex-col items-start justify-start gap-6 bg-[#0d0d14] border border-zinc-700 h-40 px-4 py-4 rounded-lg hover:opacity-80 hover:cursor-pointer">
       <p className="text-violet-400">{title}</p>
       <div className="flex flex-wrap gap-2">
         {tools.map((tools) => (
